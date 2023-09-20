b3856f3a5c5e8649dab84f4251c10f1d641564ed
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-09-20 17:32:25 +0200

    ecriture de requete simple et jointure sqlite3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-
+import sqlite3 from 'sqlite3';
 import { ads } from './data';
 
 const app = express();
-app.use(express.json());
+const db = new sqlite3.Database('good_corner.sqlite');
 
 const port: number = 3000;
+
+app.use(express.json());
+
 
 
 app.get('/', (req, res) => {
@@ -14,26 +17,55 @@ app.get('/', (req, res) => {
 });
 
 app.get('/ad', (req, res) => {
-  res.send(ads);
+  db.all('SELECT * FROM ad', (err, rows) => {
+    res.send(rows);
+  });
 });
 
 app.post('/ad', (req, res) => {
   console.log(req.body);
-  ads.push(req.body);
-  res.send("Ad has been added");
+  db.run(
+    `
+    INSERT INTO ad (title, description, owner, price, location)
+    VALUES (
+      "${req.body.title}",
+      "${req.body.description}",
+      "${req.body.owner}",
+      ${req.body.price},
+      "${req.body.ville}"
+    );
+    `
+    ,
+    () => {
+      res.send("The ad has been added");
+    }
+  )
 });
 
 app.delete('/ad', (req, res)=> {
-  // ca supprime par l'index, pas par l'id
-  ads.splice(req.body.id, 1);
-  res.send(`${req.body.id} has been delete`);
-
+  // // ca supprime par l'index, pas par l'id
+  // ads.splice(req.body.id, 1);
+  db.run(
+    `
+    DELETE FROM ad
+    `, () => {
+      res.send(`Ad has been deleted`);
+    });
 });
 
 app.put('/ad', (req, res) => {
-  ads[req.body.idToEdit] = req.body.newAdId;
-  res.send(`${req.body.idToEdit} has been modify`)
-})
+  let data = [0, '20/10/2019']
+  db.run(
+    `
+    UPDATE ad 
+    SET price= ?
+    WHERE createdAt= ?;
+    `,
+    data,
+   () => {
+    res.send(`Ad has been modify`);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
