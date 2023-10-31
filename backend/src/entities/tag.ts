5686810d8aa11df36794e9a363e2d1e2529c3fd4
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-10-31 10:07:24 +0100

    add typegraph ObjectType on entities

--- backend/src/entities/tag.ts
+++ backend/src/entities/tag.ts
@@ -1,12 +1,16 @@
 import { BaseEntity, Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ObjectType, Field, ID} from "type-graphql";
 import { Ad } from "./ad";
 
 
 @Entity()
+@ObjectType()
 export class Tag extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number
 
+  @Field(() => String)
   @Column()
   name: string;
 
