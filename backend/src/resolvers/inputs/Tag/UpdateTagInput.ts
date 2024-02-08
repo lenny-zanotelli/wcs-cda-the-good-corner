207e85e42e4491363342afff6c8deb7e4ff01326
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-08 12:30:31 +0100

    style(back): delete tag inputs

--- backend/src/resolvers/inputs/Tag/UpdateTagInput.ts
+++
@@ -1,9 +0,0 @@
-import { Length } from "class-validator";
-import { Field, InputType } from "type-graphql";
-
-@InputType()
-export class UpdateTagInput {
-  @Field()
-  @Length(2, 10)
-  name: string;
-}
