2e62c13b7952dcca7e01a5de21df5bfddad8a5c2
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-09 11:37:28 +0100

    refactor(back): crud tag entity

--- backend/src/services/tag.service.ts
+++ backend/src/services/tag.service.ts
@@ -1,11 +1,17 @@
 import datasource from "../../config/datasource";
-import { Tag } from "../entities/tag.entity";
+import { Tag, TagInput } from "../entities/tag.entity";
 import { In, Repository } from "typeorm";
 
 export class TagService {
   db: Repository<Tag>;
   constructor() {
     this.db = datasource.getRepository(Tag);
+  }
+
+  async create(data: TagInput) {
+    const newTag = this.db.create(data);
+    await this.db.save(newTag);
+    return await this.list();
   }
 
   async list(tagIds?: string[]) {
@@ -16,4 +22,25 @@ export class TagService {
     });
   }
 
+  async find(id: string) {
+    const tag = await this.db.findOne({
+      where: { id },
+    });
+    if (!tag) {
+      throw new Error("This Tag doesnt exist");
+    }
+    return tag;
+  }
+
+  async update(id: string, data: Partial<Tag>) {
+    const tag = await this.find(id);
+    const newInfos = this.db.merge(tag, data);
+    return await this.db.save(newInfos);
+  }
+  
+  async delete(id: string) {
+    const tag = await this.find(id);
+    await this.db.remove(tag);
+    return await this.list();
+  }
 }
