47d8fefa437913580cdb1d2c62ddf8b58c9f0778
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-12 16:20:13 +0100

    refactor(back):
    refacto login with message and success variable

--- backend/src/resolvers/user.resolver.ts
+++ backend/src/resolvers/user.resolver.ts
@@ -1,6 +1,10 @@
-import { User, UserInput, UserWithoutPassword } from "../entities/user.entity";
+import * as argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
+import { Message, User, UserInfo, UserInput, UserWithoutPassword } from "../entities/user.entity";
 import UserService from "../services/user.service";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import Cookies from "cookies";
+import { JWTContext } from "src";
 
 @Resolver()
 export class UserResolver {
@@ -8,6 +12,44 @@ export class UserResolver {
   async getAllUsers() {
     return await new UserService().list();
   }
+
+  @Query(() => Message)
+  async login(
+    @Arg("infos") infos: UserInput,
+    @Ctx() ctx: JWTContext
+    ) {
+    const user = await new UserService().find(infos.email);
+    if (!user) {
+      throw new Error('Verify your credentials');
+    }
+
+    const isPasswordValid = await argon2.verify(user.password, infos.password);  
+    const m = new Message();
+      if (isPasswordValid) {
+        const token = jwt.sign({
+          email: user.email,
+          role: user.role,
+          }, 
+          'secret',
+          { 
+          algorithm: 'HS256', 
+          expiresIn: '1h'
+          }
+        );
+        console.log("token", token);
+        
+        let cookies = new Cookies(ctx.req, ctx.res);
+        cookies.set("token", token, { httpOnly: true })
+        console.log("token user resolver", ctx.req.headers);
+        m.message = "Welcome";
+        m.success = true;
+    } else {
+      m.message = "Verify your credentials";
+      m.success = false;
+    }
+    return m;
+  }
+  
 
   @Mutation(() => UserWithoutPassword)
   async register(@Arg("infos") infos: UserInput) {
@@ -18,6 +60,18 @@ export class UserResolver {
     }
     const newUser = await new UserService().create(infos);
     return newUser;
+  }
+
+    @Query(() => UserInfo)
+  async whoAmI(@Ctx() ctx: JWTContext) {
+    if (ctx.user) {
+      if (ctx.user.email !== undefined) {
+        return { ...ctx.user, isLoggedIn: true };
+      } else {
+        return { isLoggedIn: false}
+      }
+    }
+    return { isLoggedIn: false }
   }
 }
 // @Resolver()
