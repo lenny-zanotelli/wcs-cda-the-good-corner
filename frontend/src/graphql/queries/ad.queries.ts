2323140d06ce8968b6044ba2f2dc6ba4c4127d92
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-14 13:56:07 +0100

    fix(front): try to fix issue with graphql document validation

--- frontend/src/graphql/queries/ad.queries.ts
+++ frontend/src/graphql/queries/ad.queries.ts
@@ -10,7 +10,6 @@ export const GET_ALL_ADS = gql`
       location
       owner
       price
-      createdAt
       category {
         id
         name
@@ -79,7 +78,6 @@ export const GET_ADS_SEARCH = gql`
       updatedAt
       category {
         id
-        name
       }
       tags {
         id
