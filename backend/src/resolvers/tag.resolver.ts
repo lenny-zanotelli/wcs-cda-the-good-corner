93069ced8eb676e1140c73c0a24c20e84a230e02
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-16 15:48:18 +0100

    feat(back): add authorized decorator on resolvers

--- backend/src/resolvers/tag.resolver.ts
+++ backend/src/resolvers/tag.resolver.ts
@@ -1,10 +1,11 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Mutation, Query, Resolver } from "type-graphql";
 import { Tag, TagInput } from "../entities/tag.entity";
 import { TagService } from "../services/tag.service";
 
 @Resolver()
 export class TagResolver {
   @Mutation(() => Tag)
+  @Authorized("admin", "manager")
   async createTag(@Arg("infos") infos: TagInput) {
     const result = await new TagService().create(infos);
     return result;
@@ -20,11 +21,13 @@ export class TagResolver {
     return await new TagService().find(id);
   }
 
+  @Authorized("admin", "manager")
   @Mutation(() => Tag)
   async updateTag(@Arg("id") id: string, @Arg("infos") infos: TagInput) {
     return await new TagService().update(id, infos);
   }
 
+  @Authorized("admin")
   @Mutation(() => Tag)
   async deleteTag(@Arg("id") id: string) {
     return await new TagService().delete(id);
