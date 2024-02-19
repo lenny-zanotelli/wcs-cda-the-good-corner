1a45be9f7719a52c64d1a288b290228c17f99d31
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-02-19 11:35:57 +0100

    refactor: fix home page when an user isnt connected

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,7 +7,9 @@ import Layout from '../components/Layout';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
   credentials: 'include',
 });
 
